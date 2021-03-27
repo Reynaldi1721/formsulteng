6cd2c115f8951a974505747fcb4cee1938a99b4e
Reynaldi1721 <71695904+Reynaldi1721@users.noreply.github.com>
2021-03-28 00:59:32 +0700

    Add files via upload
    2nd

--- process.php
+++ process.php
@@ -6,14 +6,10 @@
        $Email = $_POST['Email'];
        $Subject = $_POST['Subject'];
        $Msg = $_POST['msg'];
-       $headers =  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'From: Your name <info@address.com>' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-
 
        if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
        {
-           header('location:index.php?error');
+           header('location:index.php?error');  
        }
        else
        {
